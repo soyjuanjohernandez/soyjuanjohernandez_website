78cb23e36188ec31759a7fe7ed271b1237e7446e
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-10-08 21:52:45 -0500

    create images library

---
+++ src/assets/images/images.js
@@ -0,0 +1,30 @@
+const images = {
+    background_banner_dark: require('./background_banner_dark_theme.png'),
+    background_banner_light: require( './background_banner_light_theme.png'),
+    blog: require( './blog.svg'),
+    contacto: require( './contacto.svg'),
+    css: require( './css.svg'),
+    dark: require( './dark.svg'),
+    html: require( './html.svg'),
+    face: require( './jh_face.png'),
+    js: require( './js.svg'),
+    light: require( './light.svg'),
+    logo_banner: require( './logo_banner_color.svg'),
+    logo_banner_mono: require( './logo_banner_monocolor.svg'),
+    nav_logo_color: require( './nav_logo_color.svg'),
+    nav_logo_color_negative: require( './nav_logo_color_negative.svg'),
+    proyectos: require( './proyectos.svg'),
+    react: require( './react.svg'),
+    sass: require( './sass.svg'),
+    sobre_mi: require( './sobre_mi.svg'),
+    web_components: require( './web_components.svg'),
+    webpack: require( './webpack.svg'),
+
+    // projects
+    lookemall: require( './lookemall.png'),
+    ourclothe: require( './ourclothe.png'),
+    product_card: require( './product_card.png'),
+    social_dashboard: require( './social_dashboard.png'),
+}
+
+export default images;
