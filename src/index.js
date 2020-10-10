73254b8b37964c3f0171bf5fb1a6932d5f0c4605
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-10-09 19:43:27 -0500

    app now it´s located in pages folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './styles/index.css';
-import App from "./App";
+import App from "./pages/app/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -2,33 +2,37 @@ import React from "react";
 import "./App.css";
 
 import { Switch, Route } from "react-router-dom";
-import images from "./assets/images/images";
+import images from "../../assets/images/images";
 
-import Navbar from "./components/navbar/Navbar";
-import Banner from "./components/banner/Banner";
+import Navbar from "../../components/navbar/Navbar";
+import Banner from "../../components/banner/Banner";
 // import Banner from './components/banner/Banner'
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        {/* <Route exact path="/" component={Home} /> */}
         <Route component={Navbar} />
       </Switch>
-      <Banner image={images.logo_banner_mono}
-      imageAlt="Logotipo Soy Juanjo Hernández"
-      title="Soy Juanjo Hernández, desarrollador frontend." 
+
+      <Banner
+        image={images.logo_banner_mono}
+        imageAlt="Logotipo Soy Juanjo Hernández"
+        title="Soy Juanjo Hernández, desarrollador frontend."
       />
+
       <section id="about">
         <h1>about</h1>
       </section>
+      
       <section id="projects">
         <h1>Projects</h1>
       </section>
+
       <section id="blog">
         <h1>Blog</h1>
       </section>
-      .
+      
     </div>
   );
 }
